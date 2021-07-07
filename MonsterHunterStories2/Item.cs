a4c52190c32e183ee49c5e867eaf6d0e35739315
turtle-insect <turtle.insect@gmail.com>
2021-07-07 21:55:29 +0900

    item max count 999

--- MonsterHunterStories2/Item.cs
+++ MonsterHunterStories2/Item.cs
@@ -33,7 +33,7 @@ namespace MonsterHunterStories2
 			get { return SaveData.Instance().ReadNumber(mAddress + 2, 2); }
 			set
 			{
-				Util.WriteNumber(mAddress + 2, 2, value, 0, 99);
+				Util.WriteNumber(mAddress + 2, 2, value, 0, 999);
 				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Count)));
 			}
 		}
