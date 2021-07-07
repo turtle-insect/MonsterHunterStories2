6c10e7c131117eae7334802318d0d8b8e4ed64b5
turtle-insect <turtle.insect@gmail.com>
2021-07-08 00:56:27 +0900

    add character edit param

--- MonsterHunterStories2/Character.cs
+++ MonsterHunterStories2/Character.cs
@@ -34,6 +34,82 @@ namespace MonsterHunterStories2
 			set { Util.WriteNumber(mAddress + 234, 1, value, 1, 0xFF); }
 		}
 
-		
+		public uint SkinR
+		{
+			get { return SaveData.Instance().ReadNumber(mAddress + 64, 1); }
+			set
+			{
+				Util.WriteNumber(mAddress + 64, 1, value, 0, 255);
+				Util.WriteNumber(mAddress + 188, 1, value, 0, 255);
+			}
+		}
+
+		public uint SkinG
+		{
+			get { return SaveData.Instance().ReadNumber(mAddress + 65, 1); }
+			set
+			{
+				Util.WriteNumber(mAddress + 65, 1, value, 0, 255);
+				Util.WriteNumber(mAddress + 189, 1, value, 0, 255);
+			}
+		}
+
+		public uint SkinB
+		{
+			get { return SaveData.Instance().ReadNumber(mAddress + 66, 1); }
+			set
+			{
+				Util.WriteNumber(mAddress + 66, 1, value, 0, 255);
+				Util.WriteNumber(mAddress + 190, 1, value, 0, 255);
+			}
+		}
+
+		public uint HairStyle
+		{
+			get { return SaveData.Instance().ReadNumber(mAddress + 140, 1); }
+			set { SaveData.Instance().WriteNumber(mAddress + 140, 1, value); }
+		}
+
+		public uint Eyes
+		{
+			get { return SaveData.Instance().ReadNumber(mAddress + 168, 1); }
+			set
+			{
+				SaveData.Instance().WriteNumber(mAddress + 168, 1, value);
+				SaveData.Instance().WriteNumber(mAddress + 174, 1, value);
+			}
+		}
+
+		public uint Mouth
+		{
+			get { return SaveData.Instance().ReadNumber(mAddress + 169, 1); }
+			set { SaveData.Instance().WriteNumber(mAddress + 169, 1, value); }
+		}
+
+		public uint Makeup
+		{
+			get { return SaveData.Instance().ReadNumber(mAddress + 170, 1); }
+			set { Util.WriteNumber(mAddress + 170, 1, value, 0, 1); }
+		}
+
+		public uint BodyType
+		{
+			get { return SaveData.Instance().ReadNumber(mAddress + 180, 1); }
+			set { Util.WriteNumber(mAddress + 180, 1, value, 0, 1); }
+		}
+
+		public uint FaceShape
+		{
+			get { return SaveData.Instance().ReadNumber(mAddress + 182, 1); }
+			set { SaveData.Instance().WriteNumber(mAddress + 182, 1, value); }
+		}
+
+		public uint Voice
+		{
+			get { return SaveData.Instance().ReadNumber(mAddress + 183, 1); }
+			set { SaveData.Instance().WriteNumber(mAddress + 183, 1, value); }
+		}
+
+
 	}
 }
