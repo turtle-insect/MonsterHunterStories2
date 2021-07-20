3ae14be0b34324e934f484edd295089277448b3e
jim972329667 <59948910+jim972329667@users.noreply.github.com>
2021-07-20 14:16:32 +0800

    Support talisman info
    info\\talisman.txt

---
+++ MonsterHunterStories2/TalismanID2NameConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MonsterHunterStories2
+{
+	class TalismanID2NameConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			uint id = (uint)value;
+			String name = Info.Instance().Search(Info.Instance().Talisman, id)?.Value;
+			if (id == 0) name = "None";
+			if (String.IsNullOrEmpty(name)) name = "ID: " + id.ToString();
+			return name;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
